3c49f961a2f1060cbbcfbb8b9dcdc9d6c7720a87
Miki Kenta <mickey333ff@gmail.com>
2019-10-11 17:01:15 +0900

    [Add] binary and negative function

--- image/ImageProcessor.py
+++ image/ImageProcessor.py
@@ -4,35 +4,44 @@ import sys, select, os
 
 
 def gray(img):
-    imgArray = np.array(img)
-    im_gray = 0.299 * img[:, :, 0] + 0.587 * img[:, :, 1] + 0.114 * img[:, :, 2]
-    # for width in range(img.shape[0]):
-    #     for height in range(img.shape[1]):
-    #         pixdata = imgArray[width][height]
-    #         y = 0.229 * pixdata[0] + 0.587 * pixdata[1] + 0.114 * pixdata[2]
-    #         img[width, height] = y
-    #         # img.itemset((width, height, 0), y)
-    #         # img.itemset((width, height, 1), y)
-    #         # img.itemset((width, height, 2), y)
-    return im_gray
+    imgArray = np.asarray(img)
+    imgArray = 0.299 * imgArray[:, :, 0] + 0.587 * imgArray[:, :, 1] + 0.114 * imgArray[:, :, 2]
+    imgArray = imgArray.astype(np.uint8)
+    return imgArray
 
 def binary(img):
-    imgArray = np.array(img)
-    return img
+    imgArray = gray(img)
+    thread = 100
+    binary_img = (imgArray > thread) * 255
+    binary_img = binary_img.astype(np.uint8)
+    return binary_img
 
 def negative(img):
-    imgArray = np.array(img)
-    return img
+    imgArray = np.asarray(img)
+    imgArray = 255 - imgArray
+    return imgArray
 
 def mosaic(img):
-    imgArray = np.array(img)
+    imgArray = np.asarray(img)
+    for index in imgArray:
+        print (index[0][0], index[0][1], index[0][2])
+        print ("-----")
+        
     return img
+
+def rotation(img):
+    imgArray = np.asarray(img)
+    imgArray = imgArray[::-1]
+    return imgArray
+
 
 cam = cv2.VideoCapture(0)
 key = -1
 
 while True:
     _, img = cam.read()
+    size = (img.shape[1]//2, img.shape[0]//2)
+    img = cv2.resize(img, size)
 
     if key == int(ord('q')):
         break
@@ -43,9 +52,9 @@ while True:
     elif key == int(ord('3')):
         img = negative(img)
     elif key == int(ord('4')):
-        img = mosaic(img)
+        img = rotation(img)
     elif key == int(ord('5')):
-        img = gray(img)
+        img = mosaic(img)
     elif key == int(ord('6')):
         img = gray(img)
     else:
